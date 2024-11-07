f2676a8b267b7697f0bc1e0dedc871a092883f71
PSCD <pscd@PSCDs-MacBook-Air.local>
2024-11-07 22:38:09 +0700

    change config file

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3001";
+// export const BASE_URL = "http://localhost:3001";
 // export const BASE_URL = "https://pbl7-backend.onrender.com";
-// export const BASE_URL = "https://3479-117-2-255-206.ngrok-free.app";
+export const BASE_URL = "https://2807-42-119-65-57.ngrok-free.app";
